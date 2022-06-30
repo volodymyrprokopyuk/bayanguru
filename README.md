404208ac08ccd833e554fdf1e0c449f00c478bc0
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2022-06-30 10:59:58 +0200

    feat(sys): add stradella initial prototype

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Music scores for bayan engraved with LilyPond
+
+## Structure of the index.yaml
+
+``` yaml
+%YAML 1.2
+---
+sources:
+  - &bayan_0_1973 Баян. Підготовча група, О. Денисов, Музична Україна, 1973
+categories:
+  - &ua_folk_song Українська народна пісня
+scores:
+  - title: Ой піду я до млина # mandatory
+    from: Скарбниця народних пісень
+    composer: М. Різоль
+    arranger: А. Авдієвський
+    category: *ua_folk_song
+    source: *bayan_0_1973
+    origin: ua
+    style: [folk, classical, jazz, pop] # mandaroty (musical language)
+    genre: [song, dance, piece, study] # mandatory (musical function)
+    form: [melody, variations, study] # mandatory (musical structure)
+    texture: [monophony, homophony, polyphony] # mandatory (number of voices)
+    bass: [stradella, free_bass] # mandaroty
+    level: [elementary, intermediary, advanced, virtuoso] # mandatory
+    file: Ой-піду-я-до-млина-1kae # shuf -ern 4 {a..z} {0..9} | tr -d '\n'
+```
+
--- index.js
+++ index.js
@@ -1,5 +1,59 @@
-import { readFile } from "fs/promises"
 import { load } from "js-yaml"
+import { readFile, writeFile } from "fs/promises"
+import { $ } from "zx"
 
-const { scores } = load(await readFile("index.yaml"))
-console.log(scores)
+const chordNames = { M: "Б", m: "М", "7": "7", d: "У" }
+
+const stradellaChords = {
+  fM: "a, c f",
+  fm: "aes, c f",
+  cM: "g, e c",
+  cm: "",
+  gM: "g, b, d",
+  gm: "",
+  dM: "a, d fis",
+  dm: "",
+  aM: "a, cis e",
+  am: "",
+  eM: "gis, b, e",
+  em: "",
+  bM: "b, dis fis",
+  bm: ""
+}
+
+function stradella(score, bindBefore = true) {
+  const stPatterns = [
+    /\b(?:([cdefgab](?:es|is|eses|isis)?[,']*)\+)?/, // [bass]
+    /([cdefgab](?:es|is)?[,']*)(@)?/, // root + [bind]
+    /([Mm7d])(!)?/, // chord + [name]
+    /(\S+)?/ // [modifiers]
+  ]
+  const stNotation = new RegExp(stPatterns.map(p => p.source).join(""), 'g')
+  function lyNotation(_, bass, root, bind, chord, name, mods) {
+    const triad = stradellaChords[root.replace(/[,']*/g, "") + chord]
+    name = name ? `^${chordNames[chord]}` : ""
+    mods = mods || ""
+    chord = bass ?
+      `\\fixed c' { <${bass} ${triad}>${mods}${name} }` :
+      `\\fixed c' { <${triad}>${mods}${name} }`
+    if (bindBefore) {
+      return bind ?
+        `\\fixed c { \\once \\hide Stem \\grace \\parenthesize ${root}8 } ${chord}` :
+        chord
+    } else {
+      return bind ?
+        `\\afterGrace ${chord} { \\fixed c { \\once \\hide Stem \\parenthesize ${root}8 } }` :
+        chord
+    }
+  }
+  return String(score).replace(stNotation, lyNotation)
+}
+
+try {
+  // const { meta } = load(await readFile("index.yaml"))
+  const file = "music"
+  const score = await readFile(`${file}.lys`)
+  const stScore = stradella(score)
+  await writeFile(`${file}.ly`, stScore)
+  await $`lilypond ${file}.ly`
+} catch (e) { console.error(e) }
