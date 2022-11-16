916383906f11321e850a340a3541e3fc584b13a3
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2022-11-16 18:24:45 +0100

    feat(sys): add lintEndLineBarCheck

--- lib/validate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-STAFF="^ +<?[cdefegab](?:is|es)?[,']*"
-
-echo '** Missing first note octave check (FNOC)'
-rg $STAFF $@ | rg -v '[+@=]'
-
-echo '** Missing end line bar check (ELBC)'
-rg $STAFF $@ | rg -v '\bar "|."$' | rg '[^|{}<>]$'
