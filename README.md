e7eb3b99284610fb408231b76c68060a8246d597
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2023-02-22 11:15:00 +0100

    rfct(sys): refactor catalog functions

--- README.md
+++ README.md
@@ -8,3 +8,48 @@ Stradella bass and free bass
 ``` bash
 shuf -ern 4 {a..z} {0..9} | tr -d '\n' # picece / book random ID
 ```
+
+## Meta
+
+``` yaml
+meta:
+  # Style (sty)
+  flk: фолькльор
+  cls: класика
+  jaz: джаз
+  msc: різне
+  # Genre (gnr)
+  sng: пісня
+  dnc: танець
+  pie: п'єса
+  vls: вальс
+  plk: полька
+  mzr: мазурка
+  rmc: романс
+  stu: етюд
+  # Form (frm)
+  mel: мелодія
+  var: варіації
+  plp: поліфонія
+  scl: гами
+  arp: арпеджіо
+  wrs: повороти кисті
+  rep: репетиція
+  crd: аккорди
+  orn: мелізми
+  art: артикуляція
+  lhn: ліва рука
+  # Bass (bss)
+  stb: готовий аккорд
+  pub: чистий бас
+  frb: вибірна система
+  # Level (lvl)
+  elm: простий
+  int: середній
+  adv: складний
+  vir: віртуозний
+  # Arrangment type (aty)
+  arr: Обр. # обробка, arrangment
+  ipr: Пер. # переклад, interpratation
+  hrm: Гарм. # гармонізація, harmonization
+```
--- index.js
+++ index.js
@@ -2,35 +2,9 @@
 
 import parseArgs from "minimist"
 import { mkdir, readFile, writeFile, copyFile, access } from "fs/promises"
-// import { globby } from "globby"
-// import { marked } from "marked"
+import { chalk } from "zx"
 import { readPieces, readBooks } from "./lib/catalog.js"
 import { engravePieces, engraveBooks } from "./lib/score.js"
-import { chalk } from "zx"
-
-// async function sortPages(file) {
-//   const parsePage = page => parseInt(page.match(/-(\d+)\.svg$/)[1])
-//   const pages = await globby(`${file}@(|-+([0-9])).svg`)
-//   return pages.length > 1 ?
-//     pages.sort((a, b) => parsePage(a) - parsePage(b)) : pages
-// }
-
-// async function markupScores(scores) {
-//   const env = njk.configure(".", { autoescape: false })
-//   env.addFilter("md", marked.parseInline)
-//   const { en: t, ua } = load(await readFile("content/text.yaml"))
-//   const tasks = scores.map(async score => {
-//     const file = `${args.o}/${score.file}`
-//     score.pages = await sortPages(file)
-//     return writeFile(
-//       `${file}.html`, env.render("content/score.njk", { score, t })
-//     )
-//   })
-//   tasks.push(
-//     writeFile("index.html", env.render("content/index.njk", { scores, t }))
-//   )
-//   return tasks
-// }
 
 async function pieceInit(id) {
   const { src, file } = (await readPieces(args))[id]
@@ -63,9 +37,9 @@ async function engrave() {
 }
 
 const argsConfig = {
-  boolean: ["b", "nometa", "lint", "relax", "dry"], default: { c: "", f: "pdf" }
+  boolean: ["b", "nometa", "lint", "relax", "dry"],
+  alias: { c: "catalog", i: "init", b: "book" },
+  default: { c: "", f: "pdf" } // TODO: remove f, review c
 }
 const args = parseArgs(process.argv.slice(2), argsConfig)
 if (args.i) { await pieceInit(args.i) } else { await engrave() }
-
-// npm run serve
