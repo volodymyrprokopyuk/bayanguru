9794f3881750f5bbfb28f12c2ee07d79d8810cab
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2023-02-22 22:46:31 +0100

    rfct(sys): refactor index.js

--- index.js
+++ index.js
@@ -1,45 +1,23 @@
 #!/usr/bin/env node
 
 import parseArgs from "minimist"
-import { mkdir, readFile, writeFile, copyFile, access } from "fs/promises"
-import { chalk } from "zx"
 import { readPieces, readBooks } from "./lib/catalog.js"
-import { engravePieces, engraveBooks } from "./lib/score.js"
-
-async function pieceInit(id) {
-  const { src, file } = (await readPieces(args))[id]
-  await mkdir(`source/${src}`, { recursive: true })
-  const path = `source/${src}/${file}.lys`
-  try { await access(path); console.log(`skipping ${path} already exists`) }
-  catch { await copyFile("source/template/piece-init.lys", path) }
-}
-
-async function engrave() {
-  if (args.b) {
-    let { books, pieces } = await readBooks(args)
-    console.log(
-      chalk.blue("Catalog:"),
-      chalk.yellow.bold(`${Object.values(pieces).length} pieces`)
-    )
-    books = args._.length && args._[0] === "all" ?
-      Object.values(books) : args._.map(id => books[id])
-    await Promise.all(engraveBooks(books, pieces, args))
-  } else {
-    let pieces = await readPieces(args)
-    console.log(
-      chalk.blue("Catalog:"),
-      chalk.yellow.bold(`${Object.values(pieces).length} pieces`)
-    )
-    pieces = args._.length && args._[0] === "all" ?
-      Object.values(pieces) : args._.map(id => pieces[id])
-    await Promise.all(engravePieces(pieces, args))
-  }
-}
+import { initPieces, engravePieces, engraveBooks } from "./lib/score.js"
 
 const argsConfig = {
-  boolean: ["b", "nometa", "lint", "relax", "dry"],
+  boolean: ["i", "b", "nometa", "lint", "relax", "dry"],
   alias: { c: "catalog", i: "init", b: "book" },
-  default: { c: "", f: "pdf" } // TODO: remove f, review c
+  default: { c: "", f: "pdf" }
 }
+
 const args = parseArgs(process.argv.slice(2), argsConfig)
-if (args.i) { await pieceInit(args.i) } else { await engrave() }
+
+if (args.i) { await initPieces(args) }
+else if (args.b) {
+  const books = await readBooks(args)
+  await engraveBooks(books, args)
+}
+else {
+  const pieces = await readPieces(args)
+  await engravePieces(pieces, args)
+}
