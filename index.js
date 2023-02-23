20c35f92ebc52ac745bdd028b744e04591409338
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2023-02-23 11:27:12 +0100

    feat(sys): add jobs to engravePieces

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
+import { cpus } from "os"
 import parseArgs from "minimist"
 import { readPieces, readBooks } from "./lib/catalog.js"
 import { initPieces, engravePieces, engraveBooks } from "./lib/score.js"
 
 const argsConfig = {
   boolean: ["i", "b", "nometa", "lint", "relax", "dry"],
-  alias: { c: "catalog", i: "init", b: "book" },
-  default: { c: "", f: "pdf" }
+  alias: { c: "catalog", i: "init", b: "book", j: "jobs" },
+  default: { c: "", j: cpus().length }
 }
 
 const args = parseArgs(process.argv.slice(2), argsConfig)
