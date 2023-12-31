7b77761b6437038fc2439f5c3498b6dab5f2a70a
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2023-12-31 18:56:56 +0100

    feat(sys): add readBooks

--- internal/catalog/catalog.go
+++ internal/catalog/catalog.go
@@ -37,15 +37,6 @@ var meta = map[string]string{
   "hrm": "Гарм.", // гармонізація = harmonization
 }
 
-type PlayCommand struct {
-  Catalog string
-  Book, Cycle, Random, List, All bool
-  Pieces, Books []string
-  Queries map[string]string
-}
-
-type MatchFunc func(s string) bool
-
 type StrSlice []string
 
 func (ss *StrSlice) UnmarshalYAML(node *yaml.Node) error {
@@ -82,9 +73,46 @@ type Piece struct {
   File string
 }
 
+type PieceMap map[string]Piece
+
+type RawSection struct {
+  Tit string `yaml:"tit"`
+  Pieces []string `yaml:"pieces"`
+}
+
+type RawBook struct {
+  ID string `yaml:"id"`
+  Tit string `yaml:"tit"`
+  Sub string `yaml:"sub"`
+  Pieces []string `yaml:"pieces"`
+  Sections []RawSection `yaml:"sections"`
+}
+
+type Section struct {
+  Tit string
+  Pieces []Piece
+}
+
+type Book struct {
+  ID string
+  Tit string
+  Sub string
+  Pieces []Piece
+  Sections []Section
+}
+
+type PlayCommand struct {
+  Catalog string
+  Book, Cycle, Random, List, All bool
+  Pieces, Books []string
+  Queries map[string]string
+}
+
 func catError(format string, vals ...any) error {
   return fmt.Errorf("catalog: " + format, vals...)
 }
+
+type MatchFunc func(s string) bool
 
 func makeMatch(pattern string) (MatchFunc, error) {
   isNeg := strings.HasPrefix(pattern, "^")
@@ -129,7 +157,7 @@ func readCatalogFile(catDir, catFile string) ([]Piece, error) {
   }
   defer file.Close()
   dec := yaml.NewDecoder(file)
-  pieces := struct { Pieces []Piece `yaml:"pieces"` }{}
+  var pieces struct { Pieces []Piece `yaml:"pieces"` }
   err = dec.Decode(&pieces)
   if err != nil {
     return nil, err
@@ -161,12 +189,12 @@ func addMetaToPieces(pieces []Piece) {
   }
 }
 
-func readPieces(catDir, catQuery string) (map[string]Piece, error) {
+func readPieces(catDir, catQuery string) (PieceMap, error) {
   catFiles, err := listCatalogFiles(catDir, catQuery)
   if err != nil {
     return nil, err
   }
-  idPiece := make(map[string]Piece, 1000)
+  pieceMap := make(PieceMap, 1000)
   for _, catFile := range catFiles {
     pieces, err := readCatalogFile(catDir, catFile)
     if err != nil {
@@ -174,19 +202,116 @@ func readPieces(catDir, catQuery string) (map[string]Piece, error) {
     }
     addMetaToPieces(pieces)
     for _, piece := range pieces {
-      idPiece[piece.ID] = piece
+      pieceMap[piece.ID] = piece
     }
   }
-  return idPiece, nil
+  return pieceMap, nil
+}
+
+func readBookFile(bookDir, bookFile string) ([]RawBook, error) {
+  file, err := os.Open(filepath.Join(bookDir, bookFile))
+  if err != nil {
+    return nil, err
+  }
+  defer file.Close()
+  dec := yaml.NewDecoder(file)
+  var books struct { Books []RawBook `yaml:"books"` }
+  err = dec.Decode(&books)
+  if err != nil {
+    return nil, err
+  }
+  return books.Books, nil
+}
+
+func selectRawBooks(rawBooks []RawBook, bookIDs []string) ([]RawBook, error) {
+  rawBookMap := make(map[string]RawBook, 50)
+  for _, rb := range rawBooks {
+    rawBookMap[rb.ID] = rb
+  }
+  selRawBooks := make([]RawBook, 0, 50)
+  for _, bookID := range bookIDs {
+    if rawBook, ok := rawBookMap[bookID]; ok {
+      selRawBooks = append(selRawBooks, rawBook)
+    } else {
+      return nil, fmt.Errorf("invalid book %v", bookID)
+    }
+  }
+  return selRawBooks, nil
+}
+
+func addPiecesToBooks(rawBooks []RawBook, pieceMap PieceMap) ([]Book, error) {
+  books := make([]Book, 0, 50)
+  for _, rb := range rawBooks {
+    book := Book{ID: rb.ID, Tit: rb.Tit, Sub: rb.Sub}
+    book.Pieces = make([]Piece, 0, 200)
+    if len(rb.Sections) > 0 {
+      book.Sections = make([]Section, 0, 10)
+      for _, rs := range rb.Sections {
+        sec := Section{Tit: rs.Tit}
+        sec.Pieces = make([]Piece, 0, 20)
+        for _, pieceID := range rs.Pieces {
+          if piece, ok := pieceMap[pieceID]; ok {
+            sec.Pieces = append(sec.Pieces, piece)
+            book.Pieces = append(book.Pieces, piece)
+          } else {
+            err := fmt.Errorf(
+              "piece %v from book %v is not in catalog", pieceID, book.ID,
+            )
+            return nil, err
+          }
+        }
+      }
+    } else {
+      for _, pieceID := range rb.Pieces {
+        if piece, ok := pieceMap[pieceID]; ok {
+          book.Pieces = append(book.Pieces, piece)
+        } else {
+          err := fmt.Errorf(
+            "piece %v from book %v is not in catalog", pieceID, book.ID,
+          )
+          return nil, err
+        }
+      }
+    }
+    books = append(books, book)
+  }
+  return books, nil
+}
+
+func readBooks(
+  bookDir, bookFile string, bookIDs []string, all bool, pieceMap PieceMap,
+) ([]Book, error) {
+  rawBooks, err := readBookFile(bookDir, bookFile)
+  if err != nil {
+    return nil, err
+  }
+  if !all {
+    rawBooks, err = selectRawBooks(rawBooks, bookIDs)
+    if err != nil {
+      return nil, err
+    }
+  }
+  books, err := addPiecesToBooks(rawBooks, pieceMap)
+  if err != nil {
+    return nil, err
+  }
+  return books, nil
 }
 
 func Play(pc PlayCommand) error {
-  pieces, err := readPieces("catalog", pc.Catalog)
+
+  pieceMap, err := readPieces("catalog", pc.Catalog)
   if err != nil {
     return catError("%v", err)
   }
-  for _, piece := range pieces {
-    fmt.Println(piece)
+
+  if pc.Book {
+    books, err := readBooks("meta", "books2.yaml", pc.Books, pc.All, pieceMap)
+    if err != nil {
+      return catError("%v", err)
+    }
+    fmt.Printf("%#+v\n", books)
   }
+
   return nil
 }
